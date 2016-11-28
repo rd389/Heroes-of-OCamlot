59b31b9b336953b45827cd0fb2b51498c6518392
Ubuntu <ubuntu@csvm>
2016-11-27 22:43:31 -0500

    working on HumanPlayer.post_phase

--- players.ml
+++ players.ml
@@ -25,9 +25,21 @@ struct
   let attack_phase st =
     raise Unimplemented
   let post_phase st =
-    raise Unimplemented
+    let player = match st.first_player with
+                 | true -> fst st.players
+                 | false -> snd st.players in
+    let rec play_card p =
+      match p.mana with
+      | 0 ->
+      | m when m<0 -> play_card {p with mana = 0}
+      | m when m>0 -> let c = print_endline "Choose a card in your hand to play";
+                              read_line ()
+    in
+    play_card player
   let end_turn st =
-    raise Unimplemented
+    match st.first_player with
+    | true -> {st with first_player = false}
+    | false -> {st with first_player = true}
 end
 
 module AIPlayer : Player =
