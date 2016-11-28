911d466701729640331c94ec51eb3814dd7ebae8
Ubuntu <ubuntu@csvm>
2016-11-27 23:03:25 -0500

    finished game.play (i think)

--- _build/game.ml
+++ _build/game.ml
@@ -3,80 +3,51 @@ open State
 open Yojson.Basic.Util
 exception Unimplemented
 
-(* [get_int k j] is the int associated with the value mapped to key [k] in
- * JSON object j. *)
-let get_int k j =
-  j |> member k |> to_int
-
-(* get_string k j] is the string associated with the value mapped to key [k] in
- * JSON object j. *)
-let get_string k j =
-  j |> member k |> to_string
-
-(* [get_list k j] is the list associated with the value mapped to key [k] in
- * JSON object j. *)
-let get_list k j =
-  j |> member k |> to_list
-
-let build_bonus j =
-  let eff = match get_string "effect" j with
-            | "armor" -> Armor | "heal" -> Heal | "attack" -> Attack
-            | _ -> failwith "Invalid minion bonus effect" in
-  (eff, get_int "amount" j)
-
-let build_minion j =
-  let min = { attack =  (get_int "attack" j);
-  hp =  (get_int "health" j);
-  bonus = get_list "bonus" j |> List.map build_bonus;
-  cost = get_int "cost" j;
-  } in
-
-  { name = get_string "name" j;
-  desc = get_string "desc" j;
-  cat = Minion min
-  }
-
-let build_spell j =
-  let targ = match get_string "targ" j with
-             | "all" -> All | "me" -> Me | "them" -> Them
-             | "mine" -> Mine | "theirs" -> Theirs | "any" -> Any
-             | _ -> failwith "Invalid target" in
-  let eff = match get_string "effect" j with
-            | "heal" -> Heal | "dmg" -> Dmg | "buff" -> Buff | "mana" -> Mana
-            | _ -> failwith "Invalid spell effect" in
-  let sp = { target = targ;
-    effect = eff;
-    mag = get_int "mag" j;
-    cost = get_int "cost" j;
-    } in
-
-  { name = get_string "name" j;
-  desc = get_string "desc" j;
-  cat = Spell sp;
-  }
-
-let build_weapon j =
-  let weapon = { dmg = get_int "dmg" j;
-  durability =  (get_int "dur" j);
-  cost = get_int "cost" j;
-  } in
-
-  {name = get_string "name" j;
-  desc = get_string "desc" j;
-  cat = Weapon weapon;
-  }
-
-let shuffle deck =
-  let nd = List.map (fun c -> (Random.bits (), c)) deck in
-  let sond = List.sort compare nd in
-  List.map snd sond
-
 let deck_builder filename =
-  let j =
+
+  (* [get_int k j] is the int associated with the value mapped to key [k] in
+   * JSON object j. *)
+  let get_int k j = j |> member k |> to_int in
+  (* get_string k j] is the string associated with the value mapped to key [k]
+   * in JSON object j. *)
+  let get_string k j = j |> member k |> to_string in
+  (* [get_list k j] is the list associated with the value mapped to key [k] in
+   * JSON object j. *)
+  let get_list k j = j |> member k |> to_list in
+  let build_bonus j =
+    let eff = match get_string "effect" j with
+              | "armor" -> Armor | "heal" -> Heal | "attack" -> Attack
+              | _ -> failwith "Invalid minion bonus effect" in
+    (eff, get_int "amount" j) in
+  let build_minion j =
+    let min = {attack = (get_int "attack" j); hp = (get_int "health" j);
+                bonus = get_list "bonus" j |> List.map build_bonus; } in
+    {name = get_string "name" j; desc = get_string "desc" j;
+      cost = get_int "cost" j; cat = Minion min} in
+  let build_spell j =
+    let targ = match get_string "targ" j with
+               | "all" -> All | "me" -> Me | "them" -> Them
+               | "mine" -> Mine | "theirs" -> Theirs | "any" -> Any
+               | _ -> failwith "Invalid target" in
+    let eff = match get_string "effect" j with
+              | "heal" -> Heal | "dmg" -> Dmg | "buff" -> Buff | "mana" -> Mana
+              | _ -> failwith "Invalid spell effect" in
+    let sp = { target = targ; effect = eff; mag = get_int "mag" j} in
+    {name = get_string "name" j;desc = get_string "desc" j;
+      cost = get_int "cost" j; cat = Spell sp} in
+  let build_weapon j =
+    let weapon = {dmg = get_int "dmg" j; durability = (get_int "dur" j)} in
+    {name = get_string "name" j; desc = get_string "desc" j;
+      cost = get_int "cost" j; cat = Weapon weapon} in
+  let shuffle deck =
+    let nd = List.map (fun c -> (Random.bits (), c)) deck in
+    let sond = List.sort compare nd in
+    List.map snd sond in
+  let rec get_file f =
     try (Yojson.Basic.from_file filename) with
-    | Yojson.Json_error _ -> (print_endline "File is not of proper JSON type";
-      exit 0)
-  in
+    | _ -> (print_endline "Invalid file, try again."; print_string "> ";
+      read_line () |> get_file) in
+  let j = get_file filename in
   let minions = get_list "minions" j |> List.map build_minion in
   let spells = get_list "spells" j |> List.map build_spell in
   let weapons = get_list "weapons" j |> List.map build_weapon in
@@ -84,21 +55,21 @@ let deck_builder filename =
   shuffle deck
 
 let play (module P1 : Player) (module P2 : Player) =
-  let deck1 () =
+  let deck1 =
     print_endline "Player 1: Please enter a deck file:";
     print_string "> ";
     read_line () |> deck_builder in
-  let deck2 () =
+  let deck2 =
     ( if P2.is_human then print_endline "Player 2: Please enter a deck file:"
       else print_endline "Please enter the AI's deck file:" );
     print_string "> ";
     read_line () |> deck_builder in
   let hero1 () =
-    {hp = 30; mana = 0; attack = 0; armor = 0; hand = [];
-      deck = deck1 (); minions = []} in
+    {hp = 30; mana = 0; weap = None; armor = 0; hand = [];
+      deck = deck1; minions = []} in
   let hero2 () =
-    {hp = 30; mana = 0; attack = 0; armor = 0; hand = [];
-      deck = deck2 (); minions = []} in
+    {hp = 30; mana = 0; weap = None; armor = 0; hand = [];
+      deck = deck2; minions = []} in
   let start_state () =
     {turn = 0; first_player = true; players = (hero1 (), hero2 ())} in
   let rec play_game st =
--- _build/state.ml
+++ _build/state.ml
@@ -1,37 +1,35 @@
-(* the different possible effects for a minion card *)
 type min_effect = Armor | Heal | Attack
-(* the different possible effects for a spell card *)
+
 type sp_effect = Heal | Dmg | Buff | Mana
-(* the different possible target(s) for a spell card *)
+
 type target = All | Me | Them | Mine | Theirs | Any
-(* the type of a card from the Minion class *)
+
 type minion = {
   attack : int;
   hp : int;
   bonus : (min_effect * int) list
 }
-(* the type of a card from the Spell class *)
+
 type spell = {
   target : target;
   effect : sp_effect;
   mag : int
 }
-(* the type of a card from the Weapon class*)
+
 type weapon = {
   dmg : int;
-  durability : int;
+  durability : int
 }
-(* the different categories that a card can be *)
+
 type typ = Minion of minion | Spell of spell | Weapon of weapon
-(* the type of a card *)
+
 type card = {
   name : string;
   desc : string;
   cost : int;
   cat : typ
 }
-(* the status of player's hero, which can be altered by minions, spells, or
-  weapon cards *)
+
 type hero = {
   hp : int;
   mana : int;
@@ -41,8 +39,7 @@ type hero = {
   deck : card list;
   minions: card list
 }
-(* the game state, which can be altered by Player functions during each phase
-  of each player's turn*)
+
 type state = {
   turn : int;
   first_player : bool;
