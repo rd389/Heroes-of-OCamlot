1fb7cfff5112f427752e8060733f06fb9b873082
Ubuntu <ubuntu@csvm>
2016-11-30 18:00:00 -0500

    fixed small stuff in players.ml

--- players.ml
+++ players.ml
@@ -421,13 +421,12 @@ struct
   let post_phase st =
     let _ = Sys.command "clear" in
     print_state st;
-    let new_state = play_card st in
-    let new_player = match new_state.first_player with
-                     | true -> fst new_state.players
-                     | false -> snd new_state.players in
+    let new_player = match st.first_player with
+                     | true -> fst st.players
+                     | false -> snd st.players in
     if (new_player.weap = None && new_player.hand = [] && new_player.deck = []
     && new_player.minions = []) then raise GameOverOpp
-    else end_turn new_state
+    else end_turn st
 end
 
 module AIPlayer : Player =
@@ -752,7 +751,7 @@ struct
       List.fold_left
         (fun a c -> if (get_min_attack a) < (get_min_attack c) then c
                       else a) (List.hd ms) ms in
-    let attack_a_minion () = (Random.int 5) < 4 in
+    let attack_a_minion () = (Random.int 3) < 2 in
     let rec target ms =
       let get_target a =
         let rec attack_hero () =
